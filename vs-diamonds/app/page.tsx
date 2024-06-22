13eb5842a661412af65338c0dfe6dc0db4623902
Suryadeep Singh <suryadeep.singh098@gmail.com>
2024-06-22 15:40:26 +1000

    background image added

--- vs-diamonds/app/page.tsx
+++ vs-diamonds/app/page.tsx
@@ -1,10 +1,19 @@
 
 import Navbar from "./components/Navbar";
 import Image from "next/image";
+import background from "../public/background.jpeg"
 
 
 export default function Home() {
   return (
-    <Navbar/>
+    <div className="flex flex-col items-center justify-between">
+      <div className="relative w-full">
+        <div className="absolute -z-10 w-full">  
+          <Navbar/>
+          <Image src={background} alt="background image" className="w-full" width={1000} height={1000} />
+
+    </div>
+    </div>
+    </div>
 );
 };
